b51dd437659fe857f0a8eda10274a2b357941c13
min <side112358@gmail.com>
2016-05-28 10:13:33 +0800

    新增了gitignore
    將設定檔隱藏保護database帳密

---
+++ config_settings.example.php
@@ -0,0 +1,8 @@
+<?php
+// Enter your MySQL settings below
+$mysql_server = "localhost";
+$mysql_user = "phaos";
+$mysql_password = "phaos1234";
+$mysql_database = "phaos";
+$SITETITLE = "Min's Phaos";
+?>
